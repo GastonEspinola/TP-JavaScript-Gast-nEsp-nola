39b9118985d3b84737ebf1b23c64dd90258ca085
Gaston Espinola <gastonespinola18@gmail.com>
2024-05-10 11:11:32 -0300

    el trabajo fue finalizado

--- Trabajo_Practico_2/assets/js/notaPromedio.js
+++ Trabajo_Practico_2/assets/js/notaPromedio.js
@@ -2,12 +2,13 @@ let nota1 = parseInt(prompt('Ingrese la nota del primer trimestre'))
 let nota2 = parseInt(prompt('Ingrese la nota del segundo trimestre'))
 let nota3 = parseInt(prompt('Ingrese la nota del tercer trimestre'))
 let promedio = ((nota1+nota2+nota3)/3)
+console.log(promedio)
 
-if(nota1>10 && nota2>10 && nota3>10){
+if(nota1>10 || nota2>10 || nota3>10){
     console.log('Uno de los valores es mayor a 10')
 } else if (promedio>=1 && promedio<=3) {
     console.log('Nota insuficiente')
-} else if(promedio>=4 && promedio<=5){
+} else if(promedio>=4 && promedio<6){
     console.log('Nota regular')
 } else if(promedio>=6 && promedio<=7){
     console.log('Nota buena')
@@ -15,12 +16,8 @@ if(nota1>10 && nota2>10 && nota3>10){
     console.log('Nota muy buena')
 } else if(promedio>9 && promedio<11){
     console.log('Nota sobresaliente')
-} else if(nota1!=Number && nota2!=Number && nota3!=Number){
+} else if(nota1!=Number || nota2!=Number || nota3!=Number){
     console.log('Uno de los valores ingresados no es valido')
-} else if(nota1=isNaN && nota1>=11){
-    console.log('uno de los valores ingresados no es valido')
-} else if(nota2=isNaN && nota2>=11){
-    console.log('uno de los valores ingresados no es valido')
-} else if(nota3=isNaN && nota3>=1){
-    console.log('uno de los valores ingresados no es valido')
+} else if(isNaN(nota1) || isNaN(nota2) || isNaN(nota3)){
+    console.log('Uno de los valores no es valido')
 }
--- Trabajo_Practico_2/assets/js/tiendaDescuento.js
+++ Trabajo_Practico_2/assets/js/tiendaDescuento.js
@@ -1,12 +1,14 @@
 let articulos = parseInt(prompt('Ingrese la cantidad de articulos que lleva'))
-
-let total = articulos*20000
+let precio = parseInt(prompt('Ingrese el precio'))
+let total = articulos*precio
+let descuento = total*0.15
 
 switch (true) {
     case total>20000 && articulos>=10:
-        console.log('Se le aplicara un descuento del 15%, el total es: ')
+        console.log(`Se le aplicara un descuento del 15%, el total es: $${descuento}`)
         break;
 
     default:
+        console.log(`Usted no aplica para el descuento, su total es: $${total}`)
         break;
 }
